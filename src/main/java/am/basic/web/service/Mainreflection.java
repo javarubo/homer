d9d1716eb8acf8bd102f66034ebf2134eea64e43
Rubo <rub095088881@gmail.com>
2020-02-01 18:03:33 +0400

    frcommit

---
+++ src/main/java/am/basic/web/service/Mainreflection.java
@@ -0,0 +1,34 @@
+package am.basic.web.service;
+
+import am.basic.web.model.User;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
+public class Mainreflection {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+        Class clazz = Class.forName("am.basic.web.model.User");
+        Object object = clazz.newInstance();
+        Field field = clazz.getDeclaredField("name");
+        field.setAccessible(true);
+        field.set(object, "karen");
+
+        Field fields[] = clazz.getDeclaredFields();
+
+        for (Field fieldz : fields) {
+            System.out.println(fieldz.getName());
+
+        Annotation annotation[]=fieldz.getDeclaredAnnotations();
+            for ( Annotation annot:annotation) {
+                System.out.println(annot.annotationType());
+
+            }
+
+
+
+        }
+
+
+
+    }
+}
